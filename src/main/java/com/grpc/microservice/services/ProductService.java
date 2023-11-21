2c5d7268951f02f6b29d6f7265312092bb2fa081
Giovanni Urdaneta <giovanniurdanetatulli@gmail.com>
2023-11-20 23:26:07 -0400

    grpc server running

--- src/main/java/com/grpc/microservice/services/ProductService.java
+++ src/main/java/com/grpc/microservice/services/ProductService.java
@@ -1,14 +1,11 @@
 package com.grpc.microservice.services;
 
-import java.util.ArrayList;
-
-import org.lognet.springboot.grpc.GRpcService;
-
 import com.grpc.microservice.ProductServiceGrpc.ProductServiceImplBase;
 import com.grpc.microservice.Result;
 import com.grpc.microservice.Product.Builder;
 
 import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
 
 import com.grpc.microservice.NoParam;
 import com.grpc.microservice.Product;
@@ -16,7 +13,7 @@ import com.grpc.microservice.ProductDescription;
 import com.grpc.microservice.ProductId;
 import com.grpc.microservice.ProductList;
 
-@GRpcService
+@GrpcService
 public class ProductService extends ProductServiceImplBase {
 
     @Override
@@ -31,17 +28,14 @@ public class ProductService extends ProductServiceImplBase {
 
     @Override
     public void readProduct(NoParam request, StreamObserver<ProductList> responseObserver) {
-
         Builder p1=Product.newBuilder().setDescription("Sopa").setId(1);
 
         Builder p2=Product.newBuilder().setDescription("Refresco").setId(2);
         
-
         ProductList response = ProductList.newBuilder()
-                                    .setSuccess(true)
-                                    .setProducts(1, p1)
-                                    .setProducts(1, p2)
-                                    .build();
+                                        .setSuccess(true)                            .addProducts(p1) 
+                                        .addProducts(p2) 
+                                        .build();
 
         responseObserver.onNext(response);
         responseObserver.onCompleted();
